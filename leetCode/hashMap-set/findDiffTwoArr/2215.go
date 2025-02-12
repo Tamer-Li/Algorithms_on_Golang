848c3655cf055bb506aa6a190c3d5fe4d88e8d6c
Tamer_Li <tamer@li-work08.ru>
2025-02-13 02:30:32 +0300

    Finish 2215

--- leetCode/hashMap-set/findDiffTwoArr/2215.go
+++ leetCode/hashMap-set/findDiffTwoArr/2215.go
@@ -1,1 +1,48 @@
-package findDiffTwoArr
+package main
+
+import "fmt"
+
+func main() {
+	test1 := [][]int{
+		[]int{1, 2, 3},
+		[]int{2, 4, 6},
+	}
+
+	test2 := [][]int{
+		[]int{1, 2, 3, 3},
+		[]int{1, 1, 2, 2},
+	}
+
+	fmt.Println(findDifference(test1[0], test1[1]))
+	fmt.Println(findDifference(test2[0], test2[1]))
+}
+
+func findDifference(nums1 []int, nums2 []int) [][]int {
+	hashMap1 := make(map[int]struct{}, len(nums1))
+	hashMap2 := make(map[int]struct{}, len(nums2))
+
+	for _, v := range nums1 {
+		hashMap1[v] = struct{}{}
+	}
+
+	for _, v := range nums2 {
+		hashMap2[v] = struct{}{}
+	}
+
+	var res1 []int
+	var res2 []int
+
+	for v, _ := range hashMap1 {
+		if _, ok := hashMap2[v]; !ok {
+			res1 = append(res1, v)
+		}
+	}
+
+	for v, _ := range hashMap2 {
+		if _, ok := hashMap1[v]; !ok {
+			res2 = append(res2, v)
+		}
+	}
+
+	return [][]int{res1, res2}
+}
